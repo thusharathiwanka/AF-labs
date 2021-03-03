2049945b8ca07469493cc50607d745e4c5bc8c58
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-03-03 18:02:34 +0530

    CREATE: lab1, lab2 added

---
+++ Lab Sheet 01/script.js
@@ -0,0 +1,72 @@
+// 1
+const first = function () {
+  console.log("Hello World");
+
+  return "Thiwanka";
+  // regarding step 2 in Q1
+  // if we return this it's going to print this return
+  // otherwise it is going to be undefined when we calling the second function with first function as a parameter
+};
+
+const second = function (name) {
+  console.log(`Hi ${name}`);
+};
+
+second(first());
+
+// 2
+const user = {
+  id: "ID01",
+  name: "Thiwanka",
+  city: "Gampaha",
+  isMarried: false,
+
+  getName: function () {
+    return this.name;
+  },
+};
+
+console.log(user.getName());
+
+// 3
+window.vehicleName = "Car";
+
+const printVehicleName = function () {
+  console.log(this.vehicleName);
+};
+
+const vehicle = {
+  vehicleName: "Koenigsegg Agera",
+  getVehicleName: printVehicleName,
+};
+
+printVehicleName();
+vehicle.getVehicleName();
+console.log(this.vehicleName);
+
+// 4 (IIFE) - immediately invoked function expression
+// function that runs soon as it is defined (outer function - taxCalc)
+const taxCalc = (function () {
+  let amount = 1000;
+  let taxPercentage = 0.5;
+
+  return function () {
+    amount = taxPercentage * amount + amount;
+    return amount;
+  };
+})();
+
+console.log(taxCalc());
+
+// 5
+const apiURL = "https://jsonplaceholder.typicode.com/posts"; //getting all posts
+
+const userId = 3;
+const apiSpecificURL = `https://jsonplaceholder.typicode.com/posts/${userId}`; //getting specific post
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(JSON.stringify(data));
+  })
+  .catch((error) => console.log(error));
---
+++ Lab Sheet 02/script.js
@@ -0,0 +1,128 @@
+// 1
+// invoking callback inside the timeout
+const returnValue = (callback) => {
+  let value = 100;
+  setTimeout(() => {
+    value = 200;
+    console.log(value);
+    callback();
+  });
+  return value;
+};
+
+const printValues = () => {
+  console.log("Print something");
+};
+
+console.log(returnValue(printValues));
+
+// returning promise
+const returnPromise = () => {
+  return new Promise((resolve, reject) => {
+    let isTrue = true;
+
+    if (isTrue) {
+      resolve(100);
+    } else {
+      reject(200);
+    }
+  });
+};
+
+returnPromise()
+  .then((message) => {
+    console.log(`${message} from then.`);
+  })
+  .catch((message) => {
+    console.log(`${message} from catch.`);
+  });
+
+// 2
+function Vehicle(type) {
+  this.type = type;
+  Vehicle.VehicleCount++;
+}
+
+Vehicle.prototype.drive = function () {
+  console.log("Vehicle is driving");
+};
+
+Vehicle.VehicleCount = 0;
+
+const vehicle = new Vehicle("car");
+console.log(Vehicle.VehicleCount, vehicle.type);
+vehicle.drive();
+
+function Car(type) {
+  Vehicle.call(this, type);
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+Car.prototype.balanceWheels = function () {
+  console.log("Wheels are balanced");
+};
+
+const car = new Car("Toyota");
+console.log(car.type);
+car.balanceWheels();
+
+// 4
+const returnValue = () => {
+  let value = 100;
+  setTimeout(() => {
+    value = 200;
+    console.log(value);
+    return value;
+  });
+};
+
+(async function gettingValues() {
+  try {
+    const returnedValue = await returnValue();
+    console.log(returnedValue);
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+// 5
+class Vehicle {
+  constructor(type) {
+    // let type = something - this is private
+    // below are public
+    this.type = type;
+    Vehicle.VehicleCount++;
+  }
+  drive() {
+    console.log("Vehicle is driving");
+  }
+  static VehicleCount = 0;
+  // these will be called as a property not as a method
+  // getters and setters are acting as properties
+  get typeCombined() {
+    return this.type + " is Good";
+  }
+
+  set typeCombinedAgain(type) {
+    this.type = "Something else";
+    console.log(this.type);
+  }
+}
+
+const vehicle = new Vehicle("Car");
+console.log(vehicle.type);
+vehicle.drive();
+
+class Car extends Vehicle {
+  balanceWheels() {
+    console.log("Wheels are balanced");
+  }
+}
+
+const car = new Car("Toyota");
+console.log(car.type);
+car.balanceWheels();
+console.log(car.typeCombined);
+car.typeCombinedAgain = this.type;
