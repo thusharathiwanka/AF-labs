80cced2417c952abc5e29255aed2328f12b2ab02
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-05-21 16:08:11 +0530

    CREATE: lab11 added

---
+++ lab-sheet-11/api/posts.api.js
@@ -0,0 +1,29 @@
+const uuid = require("uuid");
+
+let posts = new Map();
+
+let createPost = (obj) => {
+  let post = {
+    id: uuid.v4(),
+    name: obj.name,
+    description: obj.description,
+    postedDate: new Date(),
+  };
+  posts.set(post.id, post);
+
+  return post;
+};
+
+let getPosts = () => {
+  return [...posts.values()];
+};
+
+let getPost = (id) => {
+  return posts.get(id);
+};
+
+module.exports = {
+  createPost,
+  getPosts,
+  getPost,
+};
